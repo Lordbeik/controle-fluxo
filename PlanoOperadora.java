ad842f131fb073035eb42c78bbcfb3226f394bb3
Carlos Antonio <gerrilhas@gmail.com>
2024-10-06 18:15:26 -0300

    primeiro commit

---
+++ PlanoOperadora.java
@@ -0,0 +1,32 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        // String plano ="M"; // M  / T
+
+        // if(plano == "B"){
+        //     System.out.println("100 minutos de ligação");
+        // } else if (plano == "M"){
+        //     System.out.println(" 100 minutos de ligação ");
+        //     System.out.println(" Whats e Instagram grátis ");
+        // }else if (plano == "T"){
+        //     System.out.println("100 minutos de ligação");
+        //     System.out.println("Whats e Instagram grátis");
+        //     System.out.println("5GB Youtube");
+        // }
+        String plano ="M";
+        switch (plano){
+            case "T":{
+                System.out.println("5Gb Youtube");
+                break;
+            }
+            case "M" : {
+                System.out.println("whats e Instagram grátis");
+                break;
+            }
+            case "B": {
+                System.out.println("100 minutos de ligação");
+            }
+        }
+
+    }
+    
+}
---
+++ src/resultadoEscolar.java
@@ -0,0 +1,16 @@
+public class resultadoEscolar {
+    public static void main(String[] args) {
+            int nota = 10;
+
+            if ( nota >= 7 )
+                System.out.println("Apavorado");
+
+                else if(nota >5 && nota<7 )
+                    System.out.println("Prova de Recuperação");
+
+                else
+                    System.out.println("reprovado");
+
+    }
+    
+}
